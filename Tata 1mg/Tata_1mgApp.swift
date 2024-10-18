f307b3c3eb80b02d7a5b3796f64bdf230c2e81f0
i-AryanGupta <aryangupta6008@gmail.com>
2024-10-18 11:58:11 +0530

    Initial Commit

---
+++ Tata 1mg/Tata_1mgApp.swift
@@ -0,0 +1,20 @@
+//
+//  Tata_1mgApp.swift
+//  Tata 1mg
+//
+//  Created by Yashom on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Tata_1mgApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
